60e4ab361027e08848f9e3c814be108f68f20df7
20CE027 <97592763+20CE027@users.noreply.github.com>
2022-03-17 10:08:56 +0530

    Create P8.py

---
+++ P8.py
@@ -0,0 +1,46 @@
+class Student:
+    def _init_(self, rollno, name, age, marks):
+        self.rollno = rollno
+        self.name = name
+        self.age = age
+        self.marks= marks
+
+
+# The Exam class adds fields representing the marks scored in three subjects.
+class Exam(Student):
+    marks = {
+        "Physics": 0,
+        "Chemistry": 0,
+        "Maths": 0,
+    }
+
+    def _init_(self, rollno, name, age, marks):
+        super()._init_(rollno, name, age)
+        self.marks = marks
+
+
+# Derive Result from the Exam class, and it has its own fields such as total_marks.
+class Result(Exam):
+    def _init_(self, rollno, name, age, marks):
+        super()._init_(rollno, name, age, marks)
+        self.total_marks = sum(self.marks.values())
+
+    def display(self):
+        print("Roll Number:", self.rollno)
+        print("Name:", self.name)
+        print("Age:", self.age)
+        print("Marks:", self.marks)
+        print("Total Marks:", self.total_marks)
+
+
+# Write an interactive program to model this relationship.
+if __name__ == "__main__":
+    rollno = int(input("Enter Roll Number: "))
+    name = input("Enter Name: ")
+    age = int(input("Enter Age: "))
+    marks = {
+        "Physics": int(input("Enter Physics Marks: ")),
+        "Chemistry": int(input("Enter Chemistry Marks: ")),
+        "Maths": int(input("Enter Maths Marks: ")),
+    }
+    print(Result._init_(rollno, name, age, marks))
